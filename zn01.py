34aba86cec4db64b45f304faba5d155133162446
chenliang <137128361@qq.com>
2020-08-28 09:09:28 +0800

    111

--- zn01.py
+++ zn01.py
@@ -1,4 +1,5 @@
 from appium import webdriver  # 导入appium第三方包
+from appiumtools import find_element
 import time
 # 1. 打开app，获取手机的把柄
 desired_caps = {}
@@ -22,4 +23,8 @@ time.sleep(5)
 driver.tap([(354,1088)])
 time.sleep(3)
 # 文本值定位
-driver.find_element_by_android_uiautomator('new UiSelector().text("我的")').click()
+# driver.find_element_by_android_uiautomator('new UiSelector().text("我的")').click()
+my_btn = ("text","我的")
+find_element(driver,my_btn).click()
+# 不过正常做app测试是不需要做登录的，直接账号输入进去之后，设置好desired_caps['noReset'] = True
+# 这个，就可以使用缓存，避免重新登录了，所以登录这个功能是不需要去做自动化的
