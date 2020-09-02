752a5800a5fa11051311f06c19bf9efa63805c29
chenliang <137128361@qq.com>
2020-09-02 17:32:37 +0800

    111

--- redis1.py
+++ redis1.py
@@ -1,9 +1,10 @@
 import redis
-# python连接redis并读取里面的token
-conn = redis.Redis(host='192.168.132.136', port=6379,password='cl2188218')
 
-p1 = conn.keys()
-for i in p1:
+# python连接redis并读取里面的token
+conn = redis.Redis(host='192.168.132.136', port=6379, password='cl2188218')
+
+p = conn.keys()
+for i in p:
     a = eval(conn.get(i))
     print(a['userinfo']['token'])
  
